d882e4bde65daf7b8e745c71abaed8996177d8fd
Agustin Puggioni <agustin.puggioni@autologica.com>
2022-12-27 10:08:45 -0300

    added: model_user & type_user

--- gql/schema.js
+++ gql/schema.js
@@ -1,15 +1,21 @@
-const { gql } = require('apollo-server')
+const { gql } = require("apollo-server");
 const typeDefs = gql`
   type User {
     id: ID
     name: String
     username: String
+    email: String
+    avatar: String
+    sitioWeb: String
+    description: String
+    psasword: String
+    createAt: String
   }
 
   type Query {
     # Users
     getUser: User
   }
-`
+`;
 
-module.exports = typeDefs
+module.exports = typeDefs;
---
+++ models/user.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true,
+  },
+  avatar: {
+    type: String,
+    trim: true,
+  },
+  sitioWeb: {
+    type: String,
+    trim: true,
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  createAt: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+module.exports = mongoose.model("User", UserSchema);
