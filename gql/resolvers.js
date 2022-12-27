d8f1616b51ed0d07be655b435c7d00de21b7ca7d
puggioni <agustinpuggioni98@gmail.com>
2022-12-27 13:35:01 -0300

    addded: mutation register

--- gql/resolvers.js
+++ gql/resolvers.js
@@ -1,3 +1,5 @@
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
 const resolvers = {
   Query: {
     //User
@@ -9,8 +11,26 @@ const resolvers = {
   Mutation: {
     //User
     register: async (_, { input }, ctx) => {
-      console.log(input);
-      return input;
+      const newUser = input;
+      newUser.email = newUser.email.toLowerCase();
+      newUser.username = newUser.username.toLowerCase();
+      //Check if user exists
+      const userFound = await User.findOne({
+        $or: [{ email: newUser.email }, { username: newUser.username }],
+      });
+      if (userFound) throw new Error("User already exists");
+      //encrypt password
+      const salt = bcrypt.genSaltSync(10);
+      newUser.password = bcrypt.hashSync(newUser.password, salt);
+      //save user
+      try {
+        const user = new User(newUser);
+        user.save();
+        console.log(user);
+        return user;
+      } catch (error) {
+        console.log(error);
+      }
     },
   },
 };
